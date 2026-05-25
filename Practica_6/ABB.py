ea58e0995ed7afee5f0a7db9404af81fda2ac079
MonAlighieri <dega2005alej@gmail.com>
2026-05-25 12:29:58 -0600

    Yacasi

--- Practica_6/ABB.py
+++ Practica_6/ABB.py
@@ -1,78 +1,50 @@
 class NodoArbol:
-    def __init__(self, clave=None, valor=None, padre=None):
-        if clave is None:
-            self.raiz = None
-        else:
-            self.clave = clave
-            self.cargaUtil = valor
-            self.hijoIzquierdo = None
-            self.hijoDerecho = None
-            self.padre = padre
+    def __init__(self, clave, valor, izquierdo=None, derecho=None, padre=None):
+        self.clave = clave
+        self.cargaUtil = valor
+        self.hijoIzquierdo = izquierdo
+        self.hijoDerecho = derecho
+        self.padre = padre
 
-    def insertar(self, clave, valor):
-        if self.raiz is not None:
-            self._insertar(clave, valor, self.raiz)
-        else:
-            self.raiz = NodoArbol(clave, valor)
 
-    def _insertar(self, clave, valor, nodoActual):
-        if clave < nodoActual.clave:
-            if nodoActual.hijoIzquierdo is not None:
-                self._insertar(clave, valor, nodoActual.hijoIzquierdo)
-            else:
-                nodoActual.hijoIzquierdo = NodoArbol(clave, valor, nodoActual)
+def abb_diccionario(secuencia):
+    vistos = {}
+    indice = {}
+    raiz = None
 
-        elif clave > nodoActual.clave:
-            if nodoActual.hijoDerecho is not None:
-                self._insertar(clave, valor, nodoActual.hijoDerecho)
-            else:
-                nodoActual.hijoDerecho = NodoArbol(clave, valor, nodoActual)
-
-        else:
-            print(f"Valor repetido:{clave}")
-
-    def imprimir_estructura(self):
-        return self._estructura(self.raiz)
-
-    def _estructura(self, nodo):
+    def insertar(nodo, clave):
         if nodo is None:
-            return ""
+            nuevo = NodoArbol(clave, clave)
+            indice[clave] = nuevo
+            return nuevo
 
-        izq = self._estructura(nodo.hijoIzquierdo)
-        der = self._estructura(nodo.hijoDerecho)
+        if clave < nodo.clave:
+            nodo.hijoIzquierdo = insertar(nodo.hijoIzquierdo, clave)
+            nodo.hijoIzquierdo.padre = nodo
+        elif clave > nodo.clave:
+            nodo.hijoDerecho = insertar(nodo.hijoDerecho, clave)
+            nodo.hijoDerecho.padre = nodo
 
-        if izq and der:
-            return f"{nodo.clave}[{izq},{der}]"
-        elif izq:
-            return f"{nodo.clave}[{izq}]"
-        elif der:
-            return f"{nodo.clave}[,{der}]"
-        else:
-            return f"{nodo.clave}"
+        return nodo
+
+    for num in secuencia:
+        if num in vistos:
+            continue
+        vistos[num] = True
+        raiz = insertar(raiz, num)
+
+    dic_final = {}
+    for clave, nodo in indice.items():
+        izq = nodo.hijoIzquierdo.clave if nodo.hijoIzquierdo else None
+        der = nodo.hijoDerecho.clave if nodo.hijoDerecho else None
+        dic_final[clave] = (izq, der)
+
+    return dic_final
 
 
-    def mostrar(self, nodo=None, nivel=0):
-        if nodo is None and nivel == 0:
-            nodo = self.raiz
-
-        if nodo is not None:
-            print("  " * nivel + " -" + str(nodo.clave))
-            self.mostrar(nodo.hijoIzquierdo, nivel + 2)
-            self.mostrar(nodo.hijoDerecho, nivel + 2)
-
-
-valores = [1, 13, 11, 5, 9, 10, 1, 12, 3, 6]
-
-arbol = NodoArbol()
-
-for v in valores:
-    arbol.insertar(v, v)
-
-print("\nÁrbol:")
-arbol.mostrar()
-
-print("Diccionario:")
-print(arbol.imprimir_estructura())
+# Ejemplo
+secuencia = [1, 13, 11, 5, 9, 10, 1, 12, 3, 6]
+print(abb_diccionario(secuencia))
 
 
 
